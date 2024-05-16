8876109839af9acdd3d1f8325c889952a81fc7c9
horriedhenry <shivahenry33@gmail.com>
2024-05-17 01:29:33 +0530

    feat : spawn food

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,53 @@
 #include <iostream>
 #include <raylib.h>
 
-#define w_height 750
-#define w_width 750
+Color Green { 173, 204, 96, 255 };
+Color DarkGreen { 43, 51, 24, 255 };
+
+int CellSize = 30;
+int CellCount = 25;
+
+class Food
+{
+public:
+    Vector2 position;
+    Texture2D food_texture;
+
+    Food()
+    {
+        Image image = LoadImage("../assets/food.png");
+        food_texture = LoadTextureFromImage(image);
+        UnloadImage(image);
+        position = GenerateRandomPosition();
+    }
+
+    Vector2 GenerateRandomPosition()
+    {
+        float x = GetRandomValue(0, CellCount - 1);
+        float y = GetRandomValue(0, CellCount - 1);
+        return Vector2{x, y};
+    }
+
+    void Draw()
+    {
+        DrawTexture(food_texture, position.x * CellSize, position.y * CellSize, WHITE);
+    }
+
+};
 
 int main (int argc, char *argv[])
 {
-    std::cout << "Retro SnakeGame" << std::endl;
-    InitWindow(w_width, w_height, "Snake");
+    InitWindow(CellSize * CellCount, CellSize * CellCount, "Snake");
+    SetTargetFPS(60);
 
     while(!WindowShouldClose()) {
         BeginDrawing();
-        ClearBackground(BLUE);
+        ClearBackground(Green);
+        Food food = Food();
+        food.Draw();
         EndDrawing();
     }
+
     CloseWindow();
     return 0;
 }
