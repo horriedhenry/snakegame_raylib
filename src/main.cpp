69d94a193c1044e14ae912c5ead93b9106cda753
horriedhenry <shivahenry33@gmail.com>
2024-05-20 15:17:36 +0530

    clang-format

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,21 @@
-#include <deque>
 #include <raylib.h>
 #include <raymath.h>
 
-Color Green { 173, 204, 96, 255 };
-Color DarkGreen { 43, 51, 24, 255 };
+#include <deque>
 
-int CellSize = 30;
+Color Green{173, 204, 96, 255};
+Color DarkGreen{43, 51, 24, 255};
+
+int CellSize  = 30;
 int CellCount = 25;
 
-double last_update_time {0};
+double last_update_time{0};
 
 // wait for certain amount of time before doing something
 bool wait(double interval)
 {
-    double current_time { GetTime() }; // Get elapsed time in seconds since InitWindow()
+    double current_time{
+        GetTime()};  // Get elapsed time in seconds since InitWindow()
     if (current_time - last_update_time >= interval) {
         last_update_time = current_time;
         return true;
@@ -31,19 +33,18 @@ bool position_in_deque(Vector2 position, std::deque<Vector2> deque)
     return false;
 }
 
-class Snake
-{
-public:
-    std::deque<Vector2> body {
+class Snake {
+   public:
+    std::deque<Vector2> body{
         Vector2{6, 9},
         Vector2{5, 9},
         Vector2{4, 9},
     };
 
-    Vector2 right {1, 0};
-    Vector2 left  {-1, 0};
-    Vector2 up    {0, -1};
-    Vector2 down  {0, 1};
+    Vector2 right{1, 0};
+    Vector2 left{-1, 0};
+    Vector2 up{0, -1};
+    Vector2 down{0, 1};
     Vector2 direction = right;
 
     Vector2 head = body.front();
@@ -52,9 +53,10 @@ public:
     void Draw()
     {
         for (auto it = body.begin(); it != body.end(); ++it) {
-            float x = it->x;
-            float y = it->y;
-            Rectangle segment = Rectangle{x * CellSize, y * CellSize, (float)CellSize, (float)CellSize};
+            float x           = it->x;
+            float y           = it->y;
+            Rectangle segment = Rectangle{x * CellSize, y * CellSize,
+                                          (float)CellSize, (float)CellSize};
             DrawRectangleRounded(segment, 0.5, 6, DarkGreen);
         }
     }
@@ -66,10 +68,7 @@ public:
         head = body.front();
     }
 
-    void Grow()
-    {
-        body.push_front(Vector2Add(body.front(), direction));
-    }
+    void Grow() { body.push_front(Vector2Add(body.front(), direction)); }
 
     void reset()
     {
@@ -79,20 +78,19 @@ public:
             Vector2{4, 9},
         };
         direction = right;
-        head = body.front();
-        tail = body.back();
+        head      = body.front();
+        tail      = body.back();
     }
 };
 
-class Food
-{
-public:
+class Food {
+   public:
     Vector2 position;
     Texture2D food_texture;
 
     Food(std::deque<Vector2> snake_body)
     {
-        Image image = LoadImage("../assets/food.png");
+        Image image  = LoadImage("../assets/food.png");
         food_texture = LoadTextureFromImage(image);
         UnloadImage(image);
         position = GenerateRandomPosition(snake_body);
@@ -116,21 +114,20 @@ public:
 
     void Draw()
     {
-        DrawTexture(food_texture, position.x * CellSize, position.y * CellSize, WHITE);
+        DrawTexture(food_texture, position.x * CellSize, position.y * CellSize,
+                    WHITE);
     }
 
     void Update(std::deque<Vector2> snake_body)
     {
         position = GenerateRandomPosition(snake_body);
     }
-
 };
 
-class Game
-{
-public:
-    Snake snake = Snake();
-    Food food = Food(snake.body);
+class Game {
+   public:
+    Snake snake  = Snake();
+    Food food    = Food(snake.body);
     bool running = true;
 
     void Draw()
@@ -143,12 +140,13 @@ public:
     {
         snake.reset();
         food.position = food.GenerateRandomPosition(snake.body);
-        running = false;
+        running       = false;
     }
 
     void CheckCollisionWithBody()
     {
-        std::deque<Vector2> head_less_body (snake.body.begin() + 1, snake.body.end());
+        std::deque<Vector2> head_less_body(snake.body.begin() + 1,
+                                           snake.body.end());
         if (position_in_deque(snake.body[0], head_less_body)) {
             GameOver();
         }
@@ -157,7 +155,8 @@ public:
     void CheckCollisionWithWalls()
     {
         // Detect Collision for wall on right and bottom
-        if (snake.head.x * CellSize >= CellSize * CellCount || snake.head.y * CellSize >= CellSize * CellCount) {
+        if (snake.head.x * CellSize >= CellSize * CellCount ||
+            snake.head.y * CellSize >= CellSize * CellCount) {
             GameOver();
         }
 
@@ -176,17 +175,16 @@ public:
             snake.Grow();
         }
     }
-
 };
 
-int main ()
+int main()
 {
     InitWindow(CellSize * CellCount, CellSize * CellCount, "Snake");
     SetTargetFPS(60);
 
     Game game = Game();
 
-    while(!WindowShouldClose()) {
+    while (!WindowShouldClose()) {
         BeginDrawing();
         ClearBackground(Green);
         if (wait(0.2)) {
@@ -197,29 +195,26 @@ int main ()
                 game.CheckCollisionWithFood();
             }
         }
-
-        if (IsKeyPressed(KEY_K) && game.snake.direction.y != 1) { // UP
-            game.running = true;
+        if (IsKeyPressed(KEY_K) && game.snake.direction.y != 1) {  // UP
+            game.running         = true;
             game.snake.direction = game.snake.up;
         }
-        if (IsKeyPressed(KEY_J) && game.snake.direction.y != -1) { // DOWN
-            game.running = true;
+        if (IsKeyPressed(KEY_J) && game.snake.direction.y != -1) {  // DOWN
+            game.running         = true;
             game.snake.direction = game.snake.down;
         }
-        if (IsKeyPressed(KEY_L) && game.snake.direction.x != -1) { // RIGHT
-            game.running = true;
+        if (IsKeyPressed(KEY_L) && game.snake.direction.x != -1) {  // RIGHT
+            game.running         = true;
             game.snake.direction = game.snake.right;
         }
-        if (IsKeyPressed(KEY_H) && game.snake.direction.x != 1) { // LEFT
-            game.running = true;
+        if (IsKeyPressed(KEY_H) && game.snake.direction.x != 1) {  // LEFT
+            game.running         = true;
             game.snake.direction = game.snake.left;
         }
         if (IsKeyPressed(KEY_Q)) {
             CloseWindow();
             exit(0);
         }
-
-
         game.Draw();
         EndDrawing();
     }
